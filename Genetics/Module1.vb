0393d4bf5fddeb7ee5a8d9bea40878a8c3a6c949
Joseito <josef.murgav@gmail.com>
2018-04-16 17:37:29 -0430

    guarda el pool fenotipo

--- Genetics/Module1.vb
+++ Genetics/Module1.vb
@@ -1,21 +1,20 @@
 ﻿Module Module1
-    Dim a() As String = {"pequeña", "mediana", "grande"}
-    Dim b() As String = {"delgado", "esbelto", "gordo"}
-    Dim c() As String = {"pequeños", "medianos", "grandes"}
-    Dim arr(,) As String
-
-    Public Sub agg(ByVal dimen As Byte, ByVal ar1() As String)
-        Dim salida(0, ar1.Length - 1) As String
-        For i As Byte = 0 To ar1.Length - 1
-            salida(0, i) = ar1(i)
-        Next
-        arr(dimen) = salida
-    End Sub
 
     Sub Main()
-        agg(0, a)
-        agg(1, b)
-        agg(2, b)
+        Dim a() As String = {"pequeña", "mediana", "grande"}
+        Dim b() As String = {"delgado", "esbelto", "gordo"}
+        Dim c() As String = {"pequeños", "medianos", "grandes"}
+        Dim arr(2, 2) As String
+
+        For i As Integer = 0 To a.Length - 1
+            arr(0, i) = a(i)
+        Next
+        For i As Integer = 0 To a.Length - 1
+            arr(1, i) = b(i)
+        Next
+        For i As Integer = 0 To a.Length - 1
+            arr(2, i) = c(i)
+        Next
         Dim perro As Creature = New Creature(arr)
         perro.pool()
         System.Console.ReadKey()

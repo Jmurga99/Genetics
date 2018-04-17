5e1d233585f85688d5701fd0a35f888db6fb6797
Joseito <josef.murgav@gmail.com>
2018-04-16 23:34:07 -0430

    Crea el genotipo

--- Genetics/Creature.vb
+++ Genetics/Creature.vb
@@ -1,14 +1,28 @@
 ﻿Public Class Creature
 
-    Private poolg As Array
+    Private poolg(,) As Integer
     Private fenotipo As Array
     Private genotipo As Array
     Private emparejar As Byte
     Private poolf(,) As String
 
+    Private Sub llenarpg(ByVal pf(,) As String)
+        Dim tofill(pf.GetLength(0) - 1, pf.GetLength(1) - 1) As Integer
+        For i As Integer = 0 To pf.GetLength(0) - 1
+            Dim vuel As Integer = 0
+            For j As Integer = 0 To pf.GetLength(1) - 1
+                tofill(i, j) = vuel
+                vuel += 1
+            Next
+
+        Next
+        poolg = tofill
+    End Sub
+
     Public Sub New(ByVal pool(,) As String)
 
         poolf = pool
+        llenarpg(poolf)
 
     End Sub
 
@@ -16,7 +30,7 @@
 
         For i As Integer = 0 To poolf.GetLength(0) - 1
             For j As Integer = 0 To poolf.GetLength(1) - 1
-                System.Console.Write(poolf(i, j) & " ")
+                System.Console.Write(poolf(i, j) & " = " & poolg(i, j) & " ")
             Next
             System.Console.WriteLine()
         Next
